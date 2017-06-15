99cc30760643c988c12c8889e0d8c33c1ca57b0a
sunmorgus <sunmorgus@gmail.com>
2017-06-15 10:20:03 -0400

    Update addon.py

--- addon.py
+++ addon.py
@@ -6,6 +6,23 @@ addonname   = addon.getAddonInfo('name')
 
 from subprocess import call
 call(["ls", "-l"])
+
+# import the XBMC libraries so we can use the controls and functions of XBMC
+import xbmc, xbmcgui
+ 
+# name and create our window 
+class BlahMainWindow(xbmcgui.Window): 
+    # and define it as self
+    def __init__(self):
+    # add picture control to our window (self) with a hardcoded path name to picture
+    self.addControl(xbmcgui.ControlImage(0,0,720,480, 'Q:\\scripts\\background.jpg'))
+ 
+# store our window as a short variable for easy of use
+W = BlahMainWindow()
+# run our window we created with our background jpeg image
+W.doModal()
+# after the window is closed, Destroy it.
+del W
  
 line1 = "Hello World!"
 line2 = "We can write anything we want here"
